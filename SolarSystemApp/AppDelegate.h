5812a00b9564619c6912e71057dbddc174cf2fbf
Sergey Rush <sergey.rush@hotmail.com>
2017-11-24 00:07:33 +0300

    Init commit

---
+++ SolarSystemApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SolarSystemApp
+//
+//  Created by Admin on 24/11/2017.
+//  Copyright © 2017 Admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SolarSystemApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SolarSystemApp
+//
+//  Created by Admin on 24/11/2017.
+//  Copyright © 2017 Admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
